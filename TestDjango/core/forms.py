ee795fa727e61432a5e4702426b3bee43421973e
Jose <82198272+ppsalas@users.noreply.github.com>
2021-11-26 12:05:34 -0300

    models migrados en sql base da datos listos con paginas funcionando

---
+++ TestDjango/core/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Usuario
+from .models import Agenda
+from .models import Citas
+from .models import Pago
+
+
+
+class CitasForm(ModelForm):
+    class Meta:
+        model = Citas
+        fields =["idagenda", "run", "hora", "estado"]
--- TestDjango/core/models.py
+++ TestDjango/core/models.py
@@ -1,3 +1,51 @@
 from django.db import models
 
 # Create your models here.
+#MODELO USUARIO
+class Usuario(models.Model):
+    run=models.CharField(max_length=10, primary_key=True, verbose_name='Rut')
+    nombre=models.CharField(max_length=20, verbose_name='Nombre')
+    email=models.CharField(max_length=50, verbose_name='Email')
+    contraseña=models.CharField(max_length=50, verbose_name='Contraseña')
+    tipo=models.CharField(max_length=50, verbose_name='Tipousuario')
+    def __str__(self):
+        return self.run
+    
+#MODELO AGENDA_MEDICO
+class Agenda(models.Model):
+    idagenda=models.IntegerField(primary_key=True, verbose_name='Id Agenda')
+    hora=models.CharField(max_length=50, verbose_name='Hora')
+    medicoespecialidad=models.CharField(null=True, max_length=50, verbose_name='Medico y especialidad')
+    run=models.CharField(max_length=10, verbose_name='Rut Medico')
+    
+    def __str__(self):
+        return self.idagenda 
+    
+#MODELO CITAS_PACIENTE
+class Citas(models.Model):
+    idagenda=models.IntegerField(primary_key=True, verbose_name='Id Agenda')
+    run=models.CharField(max_length=10, verbose_name='Rut Medico')  
+    hora=models.CharField(max_length=50, verbose_name='Hora') 
+    estado=models.CharField(max_length=50, verbose_name='NOTAS') 
+    
+    def __str__(self):
+        return self.idagenda 
+    
+#MODELO PAGO
+class Pago(models.Model):
+    idpago=models.IntegerField(primary_key=True, verbose_name='Id pago')
+    run=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    comprobante=models.CharField(max_length=50, verbose_name='Comprobante')
+    precio=models.IntegerField(verbose_name='Precio')
+    
+    def __str__(self):
+        return self.run
+    
+#MODELO INFORME
+class Informes(models.Model):
+    idinforme=models.IntegerField(primary_key=True, verbose_name='Id informe')
+    idpago=models.ForeignKey(Pago, on_delete=models.CASCADE)
+    tipoinforme=models.CharField(max_length=50, verbose_name='tipo informe')
+    
+    def __str__(self):
+        return self.idinforme 
