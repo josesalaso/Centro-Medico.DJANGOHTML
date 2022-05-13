c15411377077e6c117f30a3d98dd864d5d44b290
Jose <82198272+ppsalas@users.noreply.github.com>
2022-05-12 23:21:33 -0400

    base lista

--- TestDjango/core/admin.py
+++ TestDjango/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Usuario, Agenda, Citas, Pago
 
 # Register your models here.
+#permite administarr el modelo completo
+
+admin.site.register(Usuario)
+admin.site.register(Agenda)
+admin.site.register(Citas)
+admin.site.register(Pago)
+
+
