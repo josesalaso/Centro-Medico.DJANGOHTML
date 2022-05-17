d3ade99abc9132d72b63fc58fb2a9e781c4e85cf
Jose <82198272+ppsalas@users.noreply.github.com>
2022-05-17 11:03:59 -0400

    aaaaaa

--- TestDjango/TestDjango/settings.py
+++ TestDjango/TestDjango/settings.py
@@ -82,15 +82,10 @@ WSGI_APPLICATION = 'TestDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.oracle',
-        'NAME': 'xe',
-        'USER': 'c##pepe2',
-        'PASSWORD': 'pepe1234',
-        'HOST': '127.0.0.1',
-        'PORT': '1521',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 
 # Password validation
