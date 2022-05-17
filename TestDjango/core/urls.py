7aafb5401361e62997dd67a321c2541fccbd1fc5
Jose <82198272+ppsalas@users.noreply.github.com>
2022-05-17 11:03:11 -0400

    aaa

--- TestDjango/core/urls.py
+++ TestDjango/core/urls.py
@@ -1,18 +1,27 @@
 from django.urls import path
-from .views import index, reservarhora, pagaranularhora, confirmarhora, delhora, agregargastocomunmensual, gastocomun, interfazadministradorcondominio, interfazconcerje, agregarespacio, interfazdirectiva
+from .views import agenda, delagenda, index, reservarhora, registrar, contacto, medicos, ubicaciones, anularhora, confirmarhora, delhora, cajeropagar, comprobantepago, secretaria, modificaragendamedico, medico, agregaragenda, modificarcita, modificarcitapaciente, interfazdirectiva
 
 
 urlpatterns = [
     path('', index, name="index"),
     path('reservarhora', reservarhora, name="reservarhora"),
+    path('contacto', contacto, name="contacto"),
+    path('medicos', medicos, name="medicos"),
+    path('ubicaciones', ubicaciones, name="ubicaciones"),
     path('confirmarhora', confirmarhora, name="confirmarhora"),
-    path('pagaranularhora', pagaranularhora, name="pagaranularhora"),
+    path('anularhora', anularhora, name="anularhora"),
+    path('anularhora/<id>', anularhora, name="anularhora"),
     path('delhora/<id>', delhora, name="delhora"),
-    path('agregargastocomunmensual', agregargastocomunmensual, name="agregargastocomunmensual"),
-    path('gastocomun', gastocomun, name="gastocomun"),
-    path('interfazadministradorcondominio', interfazadministradorcondominio, name="interfazadministradorcondominio"),
-    path('interfazconcerje', interfazconcerje, name="interfazconcerje"),
-    path('agregarespacio', agregarespacio, name="agregarespacio"),
+    path('cajeropagar', cajeropagar, name="cajeropagar"),
+    path('comprobantepago', comprobantepago, name="comprobantepago"),
+    path('secretaria', secretaria, name="secretaria"),
+    path('medico', medico, name="medico"),
+    path('modificaragendamedico<id>', modificaragendamedico,name="modificaragendamedico"),
+    path('agregaragenda', agregaragenda, name="agregaragenda"),
+    path('delagenda<id>', delagenda, name="delagenda"),
+    path('agenda', agenda, name="agenda"),
+    path('modificarcita', modificarcita, name="modificarcita"),
+    path('modificarcitapaciente<id>', modificarcitapaciente, name="modificarcitapaciente"), 
     path('interfazdirectiva', interfazdirectiva, name="interfazdirectiva"),
 
 ]
--- TestDjango/core/views.py
+++ TestDjango/core/views.py
@@ -5,12 +5,22 @@ from .forms import AgendaForm, CitasForm, PagoForm
 
 def index(request):
     return render(request, 'core/index.html')
-def interfazadministradorcondominio(request):
-    return render(request, 'core/interfazadministradorcondominio.html')
+def registrar(request):
+    return render(request, 'core/registrar.html')
+def iniciarsesion(request):
+    return render(request, 'core/iniciarsesion.html')
+def contacto(request):
+    return render(request, 'core/contacto.html')
+def medicos(request):
+    return render(request, 'core/medicos.html')
+def ubicaciones(request):
+    return render(request, 'core/ubicaciones.html')
+def secretaria(request):
+    return render(request, 'core/secretaria.html')
 def interfazdirectiva(request):
     return render(request, 'core/interfazdirectiva.html')
-def interfazconcerje(request):
-    return render(request, 'core/interfazconcerje.html')
+def medico(request):
+    return render(request, 'core/medico.html')
 
 def reservarhora(request):
     # accediendo al objeto que contiene los datos de la base de datos
@@ -45,7 +55,7 @@ def validaAgenda(idagenda):
     existe=Agenda.objects.filter(idagenda=idagenda).exists()
     return existe
 
-def pagaranularhora(request):
+def anularhora(request):
         # accediendo al objeto que contiene los datos de la base de datos
     # el metodo all traera todos los productos que estan en la tabla
     citas = Citas.objects.all()
@@ -54,7 +64,7 @@ def pagaranularhora(request):
         'citas': citas
     }
     # ahora lo agregamos para que se envie al templateee
-    return render(request, 'core/pagaranularhora.html', datos)
+    return render(request, 'core/anularhora.html', datos)
 
 def delhora(request,id):
     #el id es el identificador de la tabla productos
@@ -63,9 +73,9 @@ def delhora(request,id):
     #eliminamos el producto del id buscado
     citas.delete()
     #ahora redirigmos a la pagina con el listado
-    return redirect(to="pagaranularhora")
+    return redirect(to="anularhora")
 
-def agregargastocomunmensual(request):
+def cajeropagar(request):
     # el view sera el responsable de entregar el form al template
     datos = {'form': PagoForm}
     # verificamos que peticion sean post y rescatamos los datos
@@ -82,14 +92,14 @@ def agregargastocomunmensual(request):
                 datos['mensaje'] = "AGREGADO correctamente correctamente"
             else:
                 datos['mensaje']="Ya existe un registro asociado a ese codigo "    + idpago  
-    return render(request, 'core/agregargastocomunmensual.html', datos)
+    return render(request, 'core/cajeropagar.html', datos)
 
 def validaPago(idpago):
     existe=Pago.objects.filter(idpago=idpago).exists()
     return existe
 
 
-def gastocomun(request):
+def comprobantepago(request):
     # accediendo al objeto que contiene los datos de la base de datos
     # el metodo all traera todas las horas que estan en la tabla
     pago = Pago.objects.all()
@@ -98,11 +108,11 @@ def gastocomun(request):
         'pago': pago
     }
     # ahora lo agregamos para que se envie al templateee
-    return render(request, 'core/gastocomun.html', datos)
+    return render(request, 'core/comprobantepago.html', datos)
 
 
 
-def agregarespacio(request):
+def agregaragenda(request):
     # el view sera el responsable de entregar el form al template
     datos = {'form': AgendaForm}
     # verificamos que peticion sean post y rescatamos los datos
@@ -119,8 +129,85 @@ def agregarespacio(request):
                 datos['mensaje'] = "Guardado correctamente"
             else:
                 datos['mensaje']="Ya existe un registro asociado a ese codigo"    + idagenda  
-    return render(request, 'core/agregarespacio.html', datos)
+    return render(request, 'core/agregaragenda.html', datos)
+
+
+
+def modificaragendamedico(request, id):
+
+    # el id es el identificador de la tabla productos
+    # buscando los datos en la base de datos
+    # buscamos por codigo que llega como dato en la url
+    agenda = Agenda.objects.get(idagenda=id)
+    # ahora le entregamos los datos del producto al formulario
+    datos = {'form': AgendaForm(instance=agenda)}
+
+    # verificamos que la peticion sean post y rescatamos los datos
+    if request.method == 'POST':
+        # con request recuperamos los datos del formulario y le agregamos el id modificar
+        formulario = AgendaForm(data=request.POST, instance=agenda)
+        # validamos el formulario
+        if formulario.is_valid:
+            # ahora guardamosen la base datos
+            formulario.save()
+            # enviamos mensaje
+            datos['mensaje'] = "Modificado Correctamente"
+
+    return render(request, 'core/modificaragendamedico.html', datos)
 
 
 
 
+def delagenda(request,id):
+    #el id es el identificador de la tabla productos
+    #buscando los datos en la base de datos
+    agenda=Agenda.objects.get(idagenda=id)
+    #eliminamos el producto del id buscado
+    agenda.delete()
+    #ahora redirigmos a la pagina con el listado
+    return redirect(to="agenda")
+
+def agenda(request):
+    # accediendo al objeto que contiene los datos de la base de datos
+    # el metodo all traera todas las horas que estan en la tabla
+    agenda = Agenda.objects.all()
+    # ahora crearemos una variable que le pase los datos del producto al template
+    datos = {
+        'agenda': agenda
+    }
+    # ahora lo agregamos para que se envie al templateee
+    return render(request, 'core/agenda.html', datos)
+
+
+def modificarcita(request):
+    # accediendo al objeto que contiene los datos de la base de datos
+    # el metodo all traera todas las horas que estan en la tabla
+    citas = Citas.objects.all()
+    # ahora crearemos una variable que le pase los datos del producto al template
+    datos = {
+        'citas': citas
+    }
+    # ahora lo agregamos para que se envie al templateee
+    return render(request, 'core/modificarcita.html', datos)
+
+def modificarcitapaciente(request, id):
+
+    # el id es el identificador de la tabla productos
+    # buscando los datos en la base de datos
+    # buscamos por codigo que llega como dato en la url
+    citas = Citas.objects.get(idagenda=id)
+    # ahora le entregamos los datos del producto al formulario
+    datos = {'form': CitasForm(instance=citas)}
+
+    # verificamos que la peticion sean post y rescatamos los datos
+    if request.method == 'POST':
+        # con request recuperamos los datos del formulario y le agregamos el id modificar
+        formulario = CitasForm(data=request.POST, instance=citas)
+        # validamos el formulario
+        if formulario.is_valid:
+            # ahora guardamosen la base datos
+            formulario.save()
+            # enviamos mensaje
+            datos['mensaje'] = "Modificado Correctamente"
+
+    return render(request, 'core/modificarcitapaciente.html', datos)
