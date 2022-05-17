be34289c9dba3f57b79df6dfe956684fa6df7509
Jose <82198272+ppsalas@users.noreply.github.com>
2022-05-17 11:20:34 -0400

    CENTROMEDICO VUELTAA

---
+++ TestDjango/core/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.forms import ModelForm
+from .models import Usuario
+from .models import Agenda
+from .models import Citas
+from .models import Pago
+
+
+
+class CitasForm(ModelForm):
+    class Meta:
+        model = Citas
+        fields =["idagenda", "run", "hora", "estado"]
+        
+class PagoForm(ModelForm):
+    class Meta:
+        model = Pago
+        fields =["idpago", "run", "nombrecliente", "precio"]    
+        
+        
+        
+class AgendaForm(ModelForm):
+    class Meta:
+        model = Agenda
+        fields =["idagenda", "hora", "medicoespecialidad", "run"]  
+        
+                  
